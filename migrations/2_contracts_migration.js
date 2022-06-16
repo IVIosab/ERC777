685cadd8ff5c1d743575587d6824c0d095db6d09
IVIosab <mosab.f.r@gmail.com>
2022-06-16 20:14:49 +0300

    refactor: update contract migrations script to deploy bulksend operator and have it as a default operator for AOV

--- migrations/2_contracts_migration.js
+++ migrations/2_contracts_migration.js
@@ -1,11 +1,14 @@
 const AOV = artifacts.require("./AOV.sol");
 const AOVRecipient = artifacts.require("./AOVRecipient.sol");
+const BulkSend = artifacts.require("./BulkSend.sol");
 
 require('@openzeppelin/test-helpers/configure')({ provider: web3.currentProvider, environment: 'truffle'});
 const {singletons} = require('@openzeppelin/test-helpers');
 
 module.exports = async function (deployer, network, accounts) {
-  await deployer.deploy(AOV,10000,[]);
+  await deployer.deploy(BulkSend)
+  const BulkSendOperator = await BulkSend.deployed();
+  await deployer.deploy(AOV,10000,[BulkSendOperator]);
   const token = await AOV.deployed();
   await deployer.deploy(AOVRecipient, token.address)
 };
