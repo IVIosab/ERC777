e292d6332cd979bff660c495f6bad1ed73bebb2a
IVIosab <mosab.f.r@gmail.com>
2022-06-16 20:45:16 +0300

    refactor: update contract migration script to deploy StaticPriceSeller operator and have it as a default operator for AOV

--- migrations/2_contracts_migration.js
+++ migrations/2_contracts_migration.js
@@ -1,6 +1,7 @@
 const AOV = artifacts.require("./AOV.sol");
 const AOVRecipient = artifacts.require("./AOVRecipient.sol");
 const BulkSend = artifacts.require("./BulkSend.sol");
+const StaticPriceSeller = artifacts.require("./StaticPriceSeller.sol");
 
 require('@openzeppelin/test-helpers/configure')({ provider: web3.currentProvider, environment: 'truffle'});
 const {singletons} = require('@openzeppelin/test-helpers');
@@ -8,7 +9,9 @@ const {singletons} = require('@openzeppelin/test-helpers');
 module.exports = async function (deployer, network, accounts) {
   await deployer.deploy(BulkSend);
   const BulkSendOperator = await BulkSend.deployed();
-  await deployer.deploy(AOV,10000,[BulkSendOperator.address]);
+  await deployer.deploy(StaticPriceSeller);
+  const StaticPriceSellerOperator = await StaticPriceSeller.deployed();
+  await deployer.deploy(AOV,10000,[BulkSendOperator.address,StaticPriceSellerOperator.address]);
   const token = await AOV.deployed();
   await deployer.deploy(AOVRecipient, token.address)
 };
