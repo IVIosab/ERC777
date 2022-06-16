069d4666c591149b72c294a3303f9a6e1536762a
IVIosab <mosab.f.r@gmail.com>
2022-06-16 14:37:30 +0300

    refactor: update the contracts migrations script to deploy the AOVRecipient contract

--- migrations/2_contracts_migration.js
+++ migrations/2_contracts_migration.js
@@ -1,8 +1,11 @@
 const AOV = artifacts.require("./AOV.sol");
+const AOVRecipient = artifacts.require("./AOVRecipient.sol");
 
 require('@openzeppelin/test-helpers/configure')({ provider: web3.currentProvider, environment: 'truffle'});
 const {singletons} = require('@openzeppelin/test-helpers');
 
 module.exports = async function (deployer, network, accounts) {
-  deployer.deploy(AOV,10000,[]);
+  await deployer.deploy(AOV,10000,[]);
+  const token = await AOV.deployed();
+  await deployer.deploy(AOVRecipient, token.address)
 };
