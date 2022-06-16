b1878d5ce3e48dd9ca62f13ca3e6a1b3329349a9
IVIosab <mosab.f.r@gmail.com>
2022-06-16 20:40:36 +0300

    fix: send the address of the BulkSendOperator instead of sending the contract

--- migrations/2_contracts_migration.js
+++ migrations/2_contracts_migration.js
@@ -6,9 +6,9 @@ require('@openzeppelin/test-helpers/configure')({ provider: web3.currentProvider
 const {singletons} = require('@openzeppelin/test-helpers');
 
 module.exports = async function (deployer, network, accounts) {
-  await deployer.deploy(BulkSend)
+  await deployer.deploy(BulkSend);
   const BulkSendOperator = await BulkSend.deployed();
-  await deployer.deploy(AOV,10000,[BulkSendOperator]);
+  await deployer.deploy(AOV,10000,[BulkSendOperator.address]);
   const token = await AOV.deployed();
   await deployer.deploy(AOVRecipient, token.address)
 };
