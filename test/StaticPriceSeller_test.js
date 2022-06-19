77c4ce9ba69808d7ca1e3484df09cf544ad8e00b
IVIosab <mosab.f.r@gmail.com>
2022-06-19 18:18:48 +0300

    feat: add tests for static price seller operator

---
+++ test/StaticPriceSeller_test.js
@@ -0,0 +1,39 @@
+const { accounts, contract, web3 } = require('@openzeppelin/test-environment');
+
+const { expect } = require('chai');
+require('chai').should();
+
+const { expectEvent, singletons, constants } = require('@openzeppelin/test-helpers');
+const { ZERO_ADDRESS } = constants;
+
+const AOV = contract.fromArtifact('AOV');
+const StaticPriceSeller = contract.fromArtifact('StaticPriceSeller');
+
+const data = 
+
+describe('AOV', function () {
+    const [registryFunder, creator, recipient1, recipient2, recipient3] = accounts;
+    const data = web3.utils.sha3('777TestData');
+
+    beforeEach(async function () {
+        this.timeout(3000);
+        this.erc1820 = await singletons.ERC1820Registry(registryFunder);
+        this.static = await StaticPriceSeller.new();
+        this.token = await AOV.new(10000, [this.static.address],{from: creator});
+        this.token.send(recipient1, 100, data,{from: creator});
+        this.token.send(recipient2, 100, data,{from: creator});
+    });
+
+    it('all functions work', async function () {
+        await this.static.setPricePerToken(this.token.address, 100,{from: recipient1})
+        const acc1Price = await this.static.getPricePerToken(this.token.address, recipient1);  
+        expect(acc1Price).to.eql(web3.utils.toBN(100));
+        await this.static.send(this.token.address, recipient1, {from: recipient3, value: 1000});
+        const acc1Balance = await this.token.balanceOf(recipient1);
+        const acc2Balance = await this.token.balanceOf(recipient2);
+        const acc3Balance = await this.token.balanceOf(recipient3);
+        expect(acc1Balance).to.eql(web3.utils.toBN(90));
+        expect(acc2Balance).to.eql(web3.utils.toBN(100));
+        expect(acc3Balance).to.eql(web3.utils.toBN(10));
+    });
+});
