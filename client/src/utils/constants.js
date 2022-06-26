dfe73f8ba31935b042872308c5c9e4819f1df351
IVIosab <mosab.f.r@gmail.com>
2022-06-26 20:14:06 +0300

    feat: add contract ABIs and Addresses to use in the web app

---
+++ client/src/utils/constants.js
@@ -0,0 +1,11 @@
+import AOVABI from './AOV.json';
+import BulkSendABI from './BulkSend.json';
+import StaticPriceSellerABI from './StaticPriceSeller.json';
+
+
+export const AOVabi = AOVABI.abi;
+export const AOVAddress = '0x1989B3dC839a4A5B3BE0Ab847Ff855e8e19f0496';
+export const BulkSendabi = BulkSendABI.abi;
+export const BulkSendAddress = '0x08BbA44f80F6cb3CFa39e91B6F06B52e9Dcc169F';
+export const StaticPriceSellerabi = StaticPriceSellerABI.abi;
+export const StaticPriceSellerAddress = '0x04E1Ed25b55732c31e2f5e1B1745FBadBB96E9F7';
