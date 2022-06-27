e4555fcc78d8d543d58fe4fab74eb81ec29d7762
IVIosab <mosab.f.r@gmail.com>
2022-06-27 17:58:21 +0300

    feat: add initialization process when the web page is started

--- client/staticsale/src/App.js
+++ client/staticsale/src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import detectEthereumProvider from '@metamask/detect-provider';
+import Web3 from 'web3';
+import {AOVabi, AOVAddress, StaticPriceSellerabi, StaticPriceSellerAddress} from './utils/constants';
+import {Button} from './components/Button'
+import {Input} from './components/Input'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      user: "",
+      balance: 0,
+      seller: "0xdf7fac27cf88438d5dde19169c7035dd2172d454",
+      stock: 0,
+      price: 0,
+      value: 0,
+      sendingStatus: false
+    };
+  }
+
+  initialize = async () => {
+      const provider = await detectEthereumProvider();
+      if (provider) {
+        this.web3 = new Web3(provider);
+        this.token = new this.web3.eth.Contract(AOVabi,AOVAddress);
+        this.static = new this.web3.eth.Contract(StaticPriceSellerabi,StaticPriceSellerAddress);
+        const accounts = await window.ethereum.request({method:'eth_requestAccounts'})
+        this.setState({user: accounts[0]});
+      } else {
+        alert('Please install MetaMask!');
+      }
+  }
+
+  componentDidMount(){
+    this.initialize();
+  }
+
+  render(){
+    let status;
+    if(this.state.user){
+      status = "Connected!";
+    }
+    else{
+      status = "Connect with MetaMask";
+    }
+    return (
+      <div className='gradient-bg-welcome'>
+        <div className='gradient-bg-welcome flex'>
+          <div className='flex flex-col w-1/3 justify-center items-center'>
+            <label className='text-white text-2xl'>AOV</label>
+          </div>
+          <div className='flex flex-col w-1/3 justify-center items-center'>
+            <label className='text-white text-2xl'>Bulk Sender</label>
+          </div>
+          <div className='flex flex-col w-1/3 justify-center items-center'>
+            <label className='text-white'>{status}</label>
+            <label className='text-white'>Current Account: {this.state.user.slice(0,6)}...{this.state.user.slice(this.state.user.length-4)}</label>
+            <label className='text-white'>My Balance: {this.state.balance}</label>
+          </div>
+        </div>
+      </div>
+    )  
+  }
 }
 
 export default App;
