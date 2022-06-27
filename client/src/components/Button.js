c54e61f6092fce6154f420cbd1329ebcfd501548
IVIosab <mosab.f.r@gmail.com>
2022-06-27 04:24:07 +0300

    fix: fix the button color to properly display a red button for removal and a blue button for addition or sending

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 export function Button(props){
   return(
-    <button className={`text-white bg-${props.color}-500 hover:bg-${props.color}-600 rounded m-1 px-2`} type='button' onClick={()=>props.onClick()}>{props.name}</button>
+    props.color === "blue" ?
+    <button className="text-white bg-blue-500 hover:bg-blue-600 rounded m-1 px-2" type='button' onClick={()=>props.onClick()}>{props.name}</button>
+    :
+    <button className="text-white bg-red-500 hover:bg-red-600 rounded m-1 px-2" type='button' onClick={()=>props.onClick()}>{props.name}</button>
   );
 }
