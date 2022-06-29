e107775a5c29d854432a0eba957d341299e34747
IVIosab <mosab.f.r@gmail.com>
2022-06-29 06:29:51 +0300

    refactor: update utils after redeployment to rinkeby

--- client/bulksender/src/utils/constants.js
+++ client/bulksender/src/utils/constants.js
@@ -4,8 +4,8 @@ import StaticPriceSellerABI from './StaticPriceSeller.json';
 
 
 export const AOVabi = AOVABI.abi;
-export const AOVAddress = '0x1989B3dC839a4A5B3BE0Ab847Ff855e8e19f0496';
+export const AOVAddress = '0x981640516a5A2a88f9250f42569d6a405A6279de';
 export const BulkSendabi = BulkSendABI.abi;
-export const BulkSendAddress = '0x08BbA44f80F6cb3CFa39e91B6F06B52e9Dcc169F';
+export const BulkSendAddress = '0x2c4f2FC34362e1c7E998E0134FaAb61042DDDD49';
 export const StaticPriceSellerabi = StaticPriceSellerABI.abi;
-export const StaticPriceSellerAddress = '0x04E1Ed25b55732c31e2f5e1B1745FBadBB96E9F7';
+export const StaticPriceSellerAddress = '0x689324B0C82Ce5E779b88A55B1687cFa891c6DC9';
--- truffle-config.js
+++ truffle-config.js
@@ -18,10 +18,11 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+//const HDWalletProvider = require('@truffle/hdwallet-provider');
 //
 // const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+//const mnemonic = "";
+//const infura_rinkeby_link = "";
 
 module.exports = {
   /**
@@ -45,7 +46,7 @@ module.exports = {
      host: "127.0.0.1",     // Localhost (default: none)
      port: 7545,            // Standard Ethereum port (default: none)
      network_id: "5777",       // Any network (default: none)
-    }//,
+    },
     //
     // An additional network, but with some advanced options…
     // advanced: {
@@ -59,14 +60,14 @@ module.exports = {
     //
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
-    // ropsten: {
-    //   provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    //   network_id: 3,       // Ropsten's id
-    //   gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    //   confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
-    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    //rinkeby: {
+    //  provider: () => new HDWalletProvider(mnemonic, infura_rinkeby_link),
+    //  network_id: 4,       // Ropsten's id
+    //  gas: 5500000,        // Ropsten has a lower block limit than mainnet
+    //  confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
+    //  timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    //  skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    //},
     //
     // Useful for private networks
     // private: {
